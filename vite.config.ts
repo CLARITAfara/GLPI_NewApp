646f0731a31be4bbb3650513edbc2e9744edfb53
Fiaro24 <fiaro.loyola.andria@gmail.com>
2026-06-04 10:50:04 +0300

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
-  const target = env.VITE_API_PROXY_TARGET || 'https://localhost/glpi/public'
+  const target = env.VITE_API_PROXY_TARGET || 'http://localhost'
 
   return {
     plugins: [react()],
